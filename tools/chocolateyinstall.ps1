50d8080f605c3ad722fd1dc04a9ab2120a152004
Andrei H <n3rd4i@gmail.com>
2019-11-24 16:28:58 +0100

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom/megawads/dtwid.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Join-Path $toolsDir wad)"
+  url           = $url
+  checksum      = '122B50713026A05423776E5C1330770F74A371D4236C63497F0F7F64BC9A62CB'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
